2fba57fbe28dd8302dba8ae44ee885e687029526
david musembi <dmusembi702@gmail.com>
2026-06-18 21:02:31 +0300

    Wrapped the whole app with provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { WeatherProvider } from "./contexts/WeatherContext";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <WeatherProvider>
+        <App />
+      </WeatherProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
